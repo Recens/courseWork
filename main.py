fefb57c42dafad2822c3a55772b3944f53d6dac6
Recens <d96dee22>
2020-12-17 11:40:57 +0200

    OOP

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ screen = pg.display.set_mode(size)
 pg.display.set_caption("TETRIS")
 font = pg.font.SysFont('cambria', 45)
 background = pg.image.load('res/background.jpg').convert()
+title_score = font.render("Score:", True, pg.Color("blue"))
+title_record = font.render("Record:", True, pg.Color("green"))
 
 
-class Block:
+class Parameter:
 
     figure_pos = [[(-1, 0), (-2, 0), (0, 0), (1, 0)],
                   [(0, 0), (-1, 0), (1, 0), (0, -1)],
@@ -27,8 +29,6 @@ class Block:
     score = 0
     lines = 0
     scores = {0: 0, 1: 10, 2: 30, 3: 60, 4: 100}
-    title_score = font.render("Score:", True, pg.Color("blue"))
-    title_record = font.render("Record:", True, pg.Color("green"))
 
     grid = [pg.Rect(x * BLOCK, y * BLOCK, BLOCK, BLOCK) for x in range(W) for y in range(H)]
     colors = lambda: (randrange(200, 255), randrange(200, 255), randrange(200, 255))
@@ -38,19 +38,19 @@ class Block:
 class Manager:
 
     def __init__(self):
-        self.block = Block()
+        self.parameter = Parameter()
         self.flor = [[0 for i in range(W)] for j in range(H)]
 
     def check(self, i):
 
-        if self.block.figure[i].x < 0 or self.block.figure[i].x > W - 1:
+        if self.parameter.figure[i].x < 0 or self.parameter.figure[i].x > W - 1:
             return False
-        elif self.block.figure[i].y > H - 1 or self.flor[self.block.figure[i].y][self.block.figure[i].x]:
+        elif self.parameter.figure[i].y > H - 1 or self.flor[self.parameter.figure[i].y][self.parameter.figure[i].x]:
             return False
         return True
 
 
-class Move(Block):
+class Function(Parameter):
 
     def __init__(self):
         self.manager = Manager()
@@ -62,21 +62,21 @@ class Move(Block):
         self.record = 0
 
     def move_rotation(self):
-        center = Block.figure[0]
-        figure_old = deepcopy(Block.figure)
+        center = Parameter.figure[0]
+        figure_old = deepcopy(Parameter.figure)
         if self.rotate:
             for i in range(4):
-                x = Block.figure[i].y - center.y
-                y = Block.figure[i].x - center.x
-                Block.figure[i].x = center.x - x
-                Block.figure[i].y = center.y + y
+                x = Parameter.figure[i].y - center.y
+                y = Parameter.figure[i].x - center.x
+                Parameter.figure[i].x = center.x - x
+                Parameter.figure[i].y = center.y + y
                 if not self.manager.check(i):
-                    Block.figure = deepcopy(figure_old)
+                    Parameter.figure = deepcopy(figure_old)
                     break
 
     def del_line(self):
         line = H - 1
-        Block.lines = 0
+        Parameter.lines = 0
         for row in range(H - 1, - 1, - 1):
             count = 0
             for i in range(W):
@@ -87,43 +87,43 @@ class Move(Block):
                 line -= 1
             else:
                 self.fall_speed += 1
-                Block.lines += 1
+                Parameter.lines += 1
 
     def score(self):
-        Block.score += Block.scores[Block.lines]
+        Parameter.score += Parameter.scores[Parameter.lines]
 
     def moveX(self):
-        figure_old = deepcopy(Block.figure)
+        figure_old = deepcopy(Parameter.figure)
         for i in range(4):
-            Block.figure[i].x += self.x
+            Parameter.figure[i].x += self.x
             if not self.manager.check(i):
-                Block.figure = deepcopy(figure_old)
+                Parameter.figure = deepcopy(figure_old)
                 break
 
     def move_down(self):
         self.fall_count += self.fall_speed
         if self.fall_count > self.fall_limit:
             self.fall_count = 0
-            figure_old = deepcopy(Block.figure)
+            figure_old = deepcopy(Parameter.figure)
             for i in range(4):
-                Block.figure[i].y += 1
+                Parameter.figure[i].y += 1
                 if not self.manager.check(i):
                     for i in range(4):
-                        self.manager.flor[figure_old[i].y][figure_old[i].x] = Block.color
-                    Block.color = Block.next_color
-                    Block.figure = Block.next_figure
-                    Block.next_figure = deepcopy(choice(Block.figures))
-                    Block.next_color = Block.colors()
+                        self.manager.flor[figure_old[i].y][figure_old[i].x] = Parameter.color
+                    Parameter.color = Parameter.next_color
+                    Parameter.figure = Parameter.next_figure
+                    Parameter.next_figure = deepcopy(choice(Parameter.figures))
+                    Parameter.next_color = Parameter.colors()
                     self.fall_limit = 2000
                     break
 
-    def draw_flor(self):
+    def flor(self):
         for y, row in enumerate(self.manager.flor):
             for x, col in enumerate(row):
                 if col:
-                    Block.figure_rect.x = x * BLOCK
-                    Block.figure_rect.y = y * BLOCK
-                    pg.draw.rect(screen, col, Block.figure_rect)
+                    Parameter.figure_rect.x = x * BLOCK
+                    Parameter.figure_rect.y = y * BLOCK
+                    pg.draw.rect(screen, col, Parameter.figure_rect)
 
     def get_record(self):
         try:
@@ -134,7 +134,7 @@ class Move(Block):
                 f.write('0')
 
     def set_record(self):
-        rec = max(int(self.get_record()), Block.score)
+        rec = max(int(self.get_record()), Parameter.score)
         with open('record', 'w') as f:
             f.write(str(rec))
 
@@ -145,44 +145,44 @@ class Move(Block):
                 self.set_record()
                 self.manager.flor = [[0 for i in range(W)] for i in range(H)]
                 self.anim_count, self.anim_speed, self.anim_limit = 0, 60, 2000
-                Block.score = 0
+                Parameter.score = 0
 
 
 class Draw:
     def __init__(self):
-        self.move = Move()
-        self.block = Block()
+        self.func = Function()
+        self.parameter = Parameter()
         self.manager = Manager()
 
     def draw_figure(self):
         for i in range(4):
-            self.block.figure_rect.x = self.block.figure[i].x * BLOCK
-            self.block.figure_rect.y = self.block.figure[i].y * BLOCK
-            pg.draw.rect(screen, self.block.color, self.block.figure_rect)
+            self.parameter.figure_rect.x = self.parameter.figure[i].x * BLOCK
+            self.parameter.figure_rect.y = self.parameter.figure[i].y * BLOCK
+            pg.draw.rect(screen, self.parameter.color, self.parameter.figure_rect)
 
     def draw_next_figure(self):
         for i in range(4):
-            self.block.figure_rect.x = self.block.next_figure[i].x * BLOCK + 500
-            self.block.figure_rect.y = self.block.next_figure[i].y * BLOCK + 500
-            pg.draw.rect(screen, self.block.next_color, self.block.figure_rect)
+            self.parameter.figure_rect.x = self.parameter.next_figure[i].x * BLOCK + 500
+            self.parameter.figure_rect.y = self.parameter.next_figure[i].y * BLOCK + 500
+            pg.draw.rect(screen, self.parameter.next_color, self.parameter.figure_rect)
 
     def Grid(self):
-        [pg.draw.rect(screen, (pg.Color('white')), i_rect, 1) for i_rect in self.block.grid]
+        [pg.draw.rect(screen, (pg.Color('white')), i_rect, 1) for i_rect in self.parameter.grid]
 
     def draw_score(self):
-        screen.blit(self.block.title_score, (550, 140))
-        screen.blit(font.render(str(self.block.score), True, pg.Color('white')), (720, 140))
+        screen.blit(title_score, (550, 140))
+        screen.blit(font.render(str(self.parameter.score), True, pg.Color('white')), (720, 140))
 
     def record(self):
-        screen.blit(self.block.title_record, (550, 40))
-        screen.blit(font.render(str(self.move.get_record()), True, pg.Color('yellow')), (740, 40))
+        screen.blit(title_record, (550, 40))
+        screen.blit(font.render(str(self.func.get_record()), True, pg.Color('yellow')), (740, 40))
 
 
 class GameWindow:
 
     def __init__(self):
-        self.block = Block()
-        self.move = Move()
+        self.block = Parameter()
+        self.func = Function()
         self.manager = Manager()
         self.draw = Draw()
 
@@ -193,33 +193,33 @@ class GameWindow:
 
             screen.blit(background, (0, 0))
 
-            self.move.x = 0
-            self.move.rotate = False
+            self.func.x = 0
+            self.func.rotate = False
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     quit()
                 if event.type == pg.KEYDOWN:
                     if event.key == pg.K_LEFT:
-                        self.move.x = -1
+                        self.func.x = -1
                     elif event.key == pg.K_RIGHT:
-                        self.move.x = 1
+                        self.func.x = 1
                     elif event.key == pg.K_DOWN:
-                        self.move.fall_limit = 10
+                        self.func.fall_limit = 10
                     elif event.key == pg.K_UP:
-                        self.move.rotate = True
+                        self.func.rotate = True
 
             self.draw.Grid()
             self.draw.draw_figure()
             self.draw.draw_next_figure()
             self.draw.draw_score()
             self.draw.record()
-            self.move.moveX()
-            self.move.score()
-            self.move.move_down()
-            self.move.draw_flor()
-            self.move.move_rotation()
-            self.move.del_line()
-            self.move.game_over()
+            self.func.moveX()
+            self.func.score()
+            self.func.move_down()
+            self.func.flor()
+            self.func.move_rotation()
+            self.func.del_line()
+            self.func.game_over()
 
             pg.display.flip()
             clock.tick(FPS)
